73d7d7231f9e4c82a0ad6a12086b1d4feda188d1
KarimAziev <karim.aziiev@gmail.com>
2025-04-08 17:28:00 +0300

    Version 0.2.0

--- pyimp.el
+++ pyimp.el
@@ -4,7 +4,7 @@
 
 ;; Author: Karim Aziiev <karim.aziiev@gmail.com>
 ;; URL: https://github.com/KarimAziev/pyimp
-;; Version: 0.1.0
+;; Version: 0.2.0
 ;; Keywords: languages
 ;; Package-Requires: ((emacs "29.1") (pyvenv "1.21"))
 ;; SPDX-License-Identifier: GPL-3.0-or-later
